eef5dcc7cba329b5d05649b500ee3c91f35e9072
Maykel <shalumov97@gmail.com>
2026-04-03 15:08:08 +0300

    not found page, questionCard props, dynamic routes

---
+++ src/components/NotFoundPAGE/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import classes from './NotFoundPage.module.css';
+function NotFoundPage() {
+	return (
+		<h2 className={classes.title}>
+			ERROR 404 <br />
+			Not Found PAGE
+		</h2>
+	);
+}
+
+export default NotFoundPage;
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,23 +1,23 @@
 import classes from './QuestionCard.module.css';
 import Button from '../Button';
+import { useNavigate } from 'react-router-dom';
 
-function QuestionCard() {
+function QuestionCard({ card }) {
+	const navigate = useNavigate();
 	return (
 		<div className={classes.card}>
 			<div className={classes.cardLabels}>
-				<div>Level: 1</div>
-				<div>Not Completed</div>
+				<div>Level: {card.level}</div>
+				<div>{card.completed ? 'Completed' : 'Not Completed'}</div>
 			</div>
-			<h5 className={classes.cardTitle}>Что такое JSX?</h5>
+			<h5 className={classes.cardTitle}>{card.question}</h5>
 
 			<div className={classes.cardAnswers}>
 				<label>short answer:</label>
-				<p className={classes.cardAnswer}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ex!
-				</p>
+				<p className={classes.cardAnswer}>{card.answer}</p>
 			</div>
 
-			<Button onClick={() => {}}> View </Button>
+			<Button onClick={() => navigate(`/question/${card.id}`)}> View </Button>
 		</div>
 	);
 }
