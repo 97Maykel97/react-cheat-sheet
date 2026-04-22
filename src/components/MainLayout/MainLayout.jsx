79b5b1cc63d14ad70598b1a982189b2ff37d7668
Maykel <shalumov97@gmail.com>
2026-04-22 09:28:59 +0300

    react toasts

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,21 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import classes from './MainLayout.module.css';
 import Header from '../Header/Header';
+import { ToastContainer } from 'react-toastify';
 function MainLayout() {
 	const currentYear = new Date().getFullYear();
 	return (
-		<div className={classes.mainLayout}>
-			<Header />
-			<div className={classes.mainWrapper}>
-				<main className={classes.main}>
-					<Outlet />
-				</main>
-				<footer className={classes.footer}>
-					React Cheat Sheet Application | {currentYear} <br />
-					by Maykel Shalumov
-				</footer>
+		<>
+			<div className={classes.mainLayout}>
+				<Header />
+				<div className={classes.mainWrapper}>
+					<main className={classes.main}>
+						<Outlet />
+					</main>
+					<footer className={classes.footer}>
+						React Cheat Sheet Application | {currentYear} <br />
+						by Maykel Shalumov
+					</footer>
+				</div>
 			</div>
-		</div>
+
+			<ToastContainer />
+		</>
 	);
 }
 
