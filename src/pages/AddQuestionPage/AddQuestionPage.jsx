bb2cfc2439ebb04532111d88c07d230bcb1321f3
Maykel <shalumov97@gmail.com>
2026-04-23 23:25:28 +0300

    adding new question

--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -1,15 +1,80 @@
+import { useState } from 'react';
+import { delayFn } from '../../helpers/delayFn';
 import classes from './AddQuestionPage.module.css';
+import { toast } from 'react-toastify';
+import { API_URL } from '../../constants';
 
-function QuestionPage() {
+const initialFormState = {
+	question: '',
+	answer: '',
+	description: '',
+	resources: '',
+	level: '',
+	clearForm: true,
+};
+
+function AddQuestionPage() {
+	const [formState, setFormState] = useState(initialFormState);
+	const [isPending, setIsPending] = useState(false);
+
+	const onChangeHandler = e => {
+		const { name, value, type, checked } = e.target;
+		setFormState(prev => ({
+			...prev,
+			[name]: type === 'checkbox' ? checked : value,
+		}));
+	};
+
+	const onSubmitHandler = async e => {
+		e.preventDefault();
+		try {
+			setIsPending(true);
+			await delayFn();
+
+			const resources = formState.resources.trim();
+			const response = await fetch(`${API_URL}/react`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					question: formState.question,
+					answer: formState.answer,
+					description: formState.description,
+					resources: resources.length ? resources.split(',') : [],
+					level: Number(formState.level),
+					completed: false,
+					editDate: undefined,
+				}),
+			});
+
+			if (!response.ok) {
+				throw new Error('Failed to create question');
+			}
+
+			await response.json();
+			toast.success('New question is successfully created!');
+
+			if (formState.clearForm) {
+				setFormState(initialFormState);
+			}
+		} catch (error) {
+			toast.error(error.message);
+		} finally {
+			setIsPending(false);
+		}
+	};
+
 	return (
 		<>
 			<h1 className={classes.formTitle}>Add new question</h1>
 			<div className={classes.formContainer}>
-				<form action='' className={classes.form}>
+				<form onSubmit={onSubmitHandler} className={classes.form}>
 					<div className={classes.formControl}>
 						<label htmlFor='questionField'>Question: </label>
 						<textarea
-							defaultValue={''}
+							value={formState.question}
+							onChange={onChangeHandler}
 							name='question'
 							id='questionField'
 							cols='30'
@@ -21,7 +86,8 @@ function QuestionPage() {
 					<div className={classes.formControl}>
 						<label htmlFor='answerField'>Short Answer: </label>
 						<textarea
-							defaultValue={''}
+							value={formState.answer}
+							onChange={onChangeHandler}
 							name='answer'
 							id='answerField'
 							cols='30'
@@ -33,7 +99,8 @@ function QuestionPage() {
 					<div className={classes.formControl}>
 						<label htmlFor='descriptionField'>Description: </label>
 						<textarea
-							defaultValue={''}
+							value={formState.description}
+							onChange={onChangeHandler}
 							name='description'
 							id='descriptionField'
 							cols='30'
@@ -45,7 +112,8 @@ function QuestionPage() {
 					<div className={classes.formControl}>
 						<label htmlFor='resourcesField'>Resources: </label>
 						<textarea
-							defaultValue={''}
+							value={formState.resources}
+							onChange={onChangeHandler}
 							name='resources'
 							id='resourcesField'
 							cols='30'
@@ -56,9 +124,15 @@ function QuestionPage() {
 					</div>
 					<div className={classes.formControl}>
 						<label htmlFor='levelField'>Level: </label>
-						<select name='level' id='levelField' defaultValue={'defaultValue'}>
-							<option disabled>Question level</option>
-							<hr />
+						<select
+							name='level'
+							id='levelField'
+							value={formState.level}
+							onChange={onChangeHandler}
+						>
+							<option value='' disabled>
+								Question level
+							</option>
 							<option value='1'>1 - easiest</option>
 							<option value='2'>2 - medium</option>
 							<option value='3'>3 - hardest</option>
@@ -70,15 +144,16 @@ function QuestionPage() {
 							type='checkbox'
 							name='clearForm'
 							id='clearFromField'
-							defaultValue={true}
+							checked={formState.clearForm}
+							onChange={onChangeHandler}
 						/>
 						<span>clear form after submitting?</span>
 					</label>
-					<button>Add question</button>
+					<button disabled={isPending}>Add question</button>
 				</form>
 			</div>
 		</>
 	);
 }
 
-export default QuestionPage;
+export default AddQuestionPage;
