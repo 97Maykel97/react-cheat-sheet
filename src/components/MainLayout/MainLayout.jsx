6bd8c883744f2b4f327f89ce44ee1e8a9fff5fc2
Maykel <shalumov97@gmail.com>
2026-04-24 11:09:49 +0300

    lazy components

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,7 +1,9 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import classes from './MainLayout.module.css';
 import Header from '../Header/Header';
 import { ToastContainer } from 'react-toastify';
+import { Loader } from '../Loader';
 function MainLayout() {
 	const currentYear = new Date().getFullYear();
 	return (
@@ -10,7 +12,9 @@ function MainLayout() {
 				<Header />
 				<div className={classes.mainWrapper}>
 					<main className={classes.main}>
-						<Outlet />
+						<Suspense fallback={<Loader />}>
+							<Outlet />
+						</Suspense>
 					</main>
 					<footer className={classes.footer}>
 						React Cheat Sheet Application | {currentYear} <br />
---
+++ src/pages/AddQuestionPage/AddQuestionPage.lazy.jsx
@@ -0,0 +1,7 @@
+import { lazy } from 'react';
+
+const AddQuestionPageLazy = lazy(() => {
+	import('./AddQuestionPage');
+});
+
+export default AddQuestionPageLazy;
